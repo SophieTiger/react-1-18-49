ba8698406df4a6252fa881fdf2146f171d3bf862
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-01 12:21:09 +0000

    Add conditional rendering and nesting components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 import EventsClass from "./components/EventsClass";
 import EventsFunctional from "./components/EventsFunctional";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
   return <div className="App">
-    <EventsFunctional />
-    <EventsClass />
+    {/* <EventsFunctional /> */}
+    {/* <EventsClass /> */}
+    {/* <ConditionalRenderingClass /> */}
+    {/* <ConditionalRenderingFunctional connected={true} /> */}
+    <NestingComponents />
   </div>;
 }
 
